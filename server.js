aa7b9dc2597152cd8a7fabd98002121e47a23b57
rovesea1@gmail.com <rovesea1@gmail.com>
2024-10-12 17:13:36 +0700

    fix map path use then

--- server.js
+++ server.js
@@ -1,41 +1,23 @@
 import express from "express";
 import morgan from "morgan";
-// import authRouter from './routes/auth.js'
-// import categoryRouter from "./routes/category.js";
-import cors from 'cors'
-import {readdirSync} from 'fs'
-
+import cors from 'cors';
+import { readdirSync } from 'fs';
 
 const app = express();
 const PORT = process.env.port || 3000;
 
-const readdir = readdirSync;
-
 // middleware
-
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
-//Router
+// Automatically load all routers in the routes directory
+readdirSync('./routes').map((file) => {
+    import(`./routes/${file}`).then((module) => {
+        app.use('/api', module.default);
+    }).catch((err) => console.error(`Failed to load ${file}:`, err));
+});
 
-// app.use('/api', authRouter);
-// app.use('/api', categoryRouter);
-
-// console.log(readdir(`./routes`));
-
-
-readdir('./routes')
-    .map((item) => app.use('/api', ('./routes/' + item)))
-
-
-
-
-
-
-
-
-//start server
-app.listen( PORT , () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
