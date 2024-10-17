f3c62ecf997fc20388c025dab9429f078580e841
rovesea1@gmail.com <rovesea1@gmail.com>
2024-10-17 23:47:53 +0700

    save point

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { register, login, currentUser, currentAdmin, protectedToken } from "../controllers/auth.js";
+import { authenticateToken } from '../middleware/authMiddleWare.js'
 
 const router = express.Router();
 
 router.post(`/register`, register);
-router.post(`/login`, login);
+router.post(`/login`, authenticateToken, login);
 router.post(`/current-user`, currentUser);
 router.post(`/current-admin`, currentAdmin);
 router.get(`/protected`, protectedToken);
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cors from 'cors';
 import { readdirSync } from 'fs';
+import dotenv from 'dotenv'
 
 const app = express();
 const PORT = process.env.port || 3000;
@@ -10,6 +11,7 @@ const PORT = process.env.port || 3000;
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
+dotenv.config();
 
 // Automatically load all routers in the routes directory
 readdirSync('./routes').map((file) => {
