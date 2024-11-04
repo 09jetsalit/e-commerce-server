42d9b12b97d20671c378a8c45ec11bc292d64fc2
rovesea1@gmail.com <rovesea1@gmail.com>
2024-11-04 23:23:32 +0700

    a

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { register, login, currentUser, currentAdmin, token } from "../controllers/authController.js";
-
+import {authenticateToken, adminCheck} from '../middleware/authMiddleWare.js'
 const router = express.Router();
 
 
 router.post(`/register`, register);
 router.post(`/login`, login);
-router.post(`/current-user`, currentUser);
-router.post(`/current-admin`, currentAdmin);
+router.post(`/current-user`, authenticateToken, currentUser);
+router.post(`/current-admin`, authenticateToken, adminCheck, currentAdmin);
 router.post(`/token`, token);
 
 export default router;
